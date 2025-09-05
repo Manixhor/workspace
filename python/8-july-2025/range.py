6285957672144fd5b71ea9a0ff14ea3119b04372
Manixhor <manigururam@hotmail.com>
2025-09-05 22:02:36 +0530

    adding

---
+++ python/8-july-2025/range.py
@@ -0,0 +1,19 @@
+#how to print odd numbers and even no's using range function ?
+print("Odd no's")
+for i in range(1,100,2):
+   
+    print(i)
+
+print("even no's")
+for i in range(2,100,2):
+   
+    print(i)
+
+
+#printing no's without -1 time ?
+print("logic for range -1")
+for i in range(1,21+1):
+    print(i)
+
+for i in range(22)[1:]:
+    print(i)
