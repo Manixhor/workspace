185f08285c19783a25c052f1d7aba05af7107954
Manixhor <manigururam@hotmail.com>
2025-09-25 18:13:09 +0530

    adding new files

---
+++ works/25-sept-2025/5file/main.py
@@ -0,0 +1,55 @@
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+    origin: str
+
+# In-memory store for items
+items_db = {}
+
+
+class Student(BaseModel):
+    name: str
+    age: int
+    grade: str
+
+@app.get("/students/")
+def get_all_students():
+    return [{"name": "Dheeraj23", "age": 20, "grade": "A++"}, {"name": "Sashi", "age": 22, "grade": "B++"}]   
+
+
+
+
+
+@app.get("/items/")
+def get_all_items():
+    return list(items_db.values())
+
+@app.post("/items/")
+def create_item(item: Item):
+    items_db[item.name] = item
+    return item
+
+@app.get("/items/{name}")
+def get_item(name: str):
+    result = items_db.get(name)
+    if result:
+        return result
+    return {"error": "Item not found check very keenly"}
+
+@app.put("/items/{name}")
+def update_item(name: str, item: Item):
+    items_db[name] = item
+    return item
+@app.delete("/items/{name}")
+def delete_item(name: str):
+    if name in items_db:
+        deleted_item = items_db.pop(name)
+        return {"message": f"Item {name} deleted", "item": deleted_item}
+    return {"error": f"Item {name} not found"}
