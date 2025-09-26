1e7680d6ff8232e55eb204cce677d82280fb2f28
Manixhor <manigururam@hotmail.com>
2025-09-26 17:02:05 +0530

    added codes in interview

---
+++ que/fil3/maxium.py
@@ -0,0 +1,13 @@
+
+
+maximum = [1,2,3,4,5]
+
+
+biggest = maximum[0]
+for num in maximum:
+    if num < biggest:
+
+        biggest = num
+
+print(biggest)
+
--- que/fil3/palin.py
+++
@@ -1,8 +0,0 @@
-num =[1, 2, 2, 3, 4, 4, 5]
-
-result = []
-for i in num:
-    if i not in result:
-        result.append(i)
-
-print(result)
