b26e334460a741f59e2f0f64d53667951d3afa52
Manixhor <manigururam@hotmail.com>
2025-11-14 08:04:23 +0530

    added sessionid codes

--- python/fib.py
+++ python/fib.py
@@ -1,11 +1,11 @@
-# n = int(input())
 
-# a = 0 
-# b = 1
-# for i in range(n):
-   
-#     c = a + b
-#     print(c)
-#     a = b 
-#     b = c
+n = int(input())
 
+a = 0
+b = 1
+
+for i in range(n):
+    c = a + b 
+    print(c)
+    a = b 
+    b = c 
