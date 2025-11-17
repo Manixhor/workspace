928c8a686eb7cffe96d6ae85cf7b70c2cbe66fbc
Manixhor <manigururam@hotmail.com>
2025-11-17 22:00:18 +0530

    added today's works

--- FastAPI/17-nov/nested/main2.py
+++ FastAPI/17-nov/nested/main2.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+class items(BaseModel):
+    name : str 
+    price: float 
+    availability:Optional[bool] = None
 
 app = FastAPI()
 
-@app.get("/display/{item:id}")
-def view(id:int):
-    return f"{id}"
+@app.post("/display")
+def view(data:items):
+    return {"message":"Item received","data":data}
 
+
+
+#request body --> used to send the structured data to server
+#pydantic model --> validation serialization documentation 
