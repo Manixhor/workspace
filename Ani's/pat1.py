3c4c323de196f3c064b21819bb355de0480c4026
Manixhor <manigururam@hotmail.com>
2025-12-09 17:25:11 +0530

    added some codes

---
+++ Ani's/pat1.py
@@ -0,0 +1,21 @@
+
+r = 3
+c = 13
+
+for i in range(r):
+    for j in range(c):
+        print("*",end="")
+        if j != c-1:
+            print("_",end="")
+    print()
+
+r = 10
+c = 3 
+
+for i in range(r):
+    for j in range(c):
+        print("*",end="")
+        if j != c - 1:
+            print("_",end="")
+    print()
+
---
+++ Ani's/patterns.py
@@ -0,0 +1,7 @@
+# print("hello",end=" ")
+# print("Mani")
+
+li = ["hello","vish","mani"]
+
+for i in range(len(li)):
+    print(li[i],end=",")
